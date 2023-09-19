ebb8836c704293da808dc737c8cdf33e0dba73b0
Danill13and <dandros01@gmail.com>
2023-09-19 17:44:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const name = 'Danil'
+const sureName = 'Androsov'
+let age = 16
+let hobby = 'Playing on guitar'
+
+console.log(`мене звати "${name}" "${sureName}" мені "${age}" років, у свободний час я полюбляю ${hobby}`)
